1d3f5e06a290dac11d5e4b27e2e990df2b2207d3
tommaso <tommaso.botarelli@stud.unifi.it>
2020-11-04 23:29:56 +0100

    Classe eventview

--- eventview.cpp
+++ eventview.cpp
@@ -1,10 +1,20 @@
 #include "eventview.h"
 #include "ui_eventview.h"
 
-EventView::EventView(QWidget *parent) :
-        QDialog(parent),
+EventView::EventView(Event *e, CalendarController *c, QWidget *parent) :
+        event(e), controller(c), QDialog(parent),
         ui(new Ui::EventView) {
     ui->setupUi(this);
+    setWindowTitle(event->getTask());
+
+    ui->dateEdit->setDate(event->getDate());
+    ui->StartTimeEdit->setTime(event->getStartTime());
+    ui->EndTimeEdit->setTime(event->getEndTime());
+    ui->NoteEdit->setText(event->getNote());
+
+    if (event->isAllDay())
+        ui->AllDayCheckBox->setCheckState(Qt::Checked);
+
 }
 
 EventView::~EventView() {
@@ -12,5 +22,6 @@ EventView::~EventView() {
 }
 
 void EventView::on_DeleteButton_clicked() {
-
+    controller->remove();
+    this->close();
 }
