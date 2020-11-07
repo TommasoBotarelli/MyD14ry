711a44da9cb46f3e2c244dda263a4984dce6c217
marco <marco.deluca4@stud.unifi.it>
2020-11-07 13:04:05 +0100

    completata ListOfShoppingListController, test completato e valido di ShoppingListController.

---
+++ ListOfShoppingListController.h
@@ -0,0 +1,27 @@
+//
+// Created by marco on 07/11/20.
+//
+
+#ifndef MYD14RY_LISTOFSHOPPINGLISTCONTROLLER_H
+#define MYD14RY_LISTOFSHOPPINGLISTCONTROLLER_H
+
+#include "ShoppingList.h"
+#include "ListOfShoppingList.h"
+#include <QString>
+
+class ListOfShoppingListController {
+
+public:
+    ListOfShoppingListController(ShoppingList *s,ListOfShoppingList *listOfShopList):shoppingList(s),listOfShoppingList(listOfShopList){}
+
+    void setData(QString name,QString product);
+
+    void remove();
+private:
+    ListOfShoppingList *listOfShoppingList;
+
+    ShoppingList *shoppingList;
+};
+
+
+#endif //MYD14RY_LISTOFSHOPPINGLISTCONTROLLER_H
