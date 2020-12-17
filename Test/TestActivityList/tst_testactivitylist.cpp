31ecdc705462c5c1222904fd8bc89833f66afc68
marco <marco.deluca4@stud.unifi.it>
2020-12-17 13:09:31 +0100

    completare aggiornamento Test activity list (removeActivity,...)

--- Test/TestActivityList/tst_testactivitylist.cpp
+++ Test/TestActivityList/tst_testactivitylist.cpp
@@ -2,6 +2,7 @@
 #include "../../Activity.h"
 #include "../../ActivityList.h"
 #include "../../SubActivity.h"
+#include "../../Category.h"
 
 // add necessary includes here
 
@@ -17,7 +18,7 @@ private slots:
 
     void testAddGetActivity();
 
-    void testRemoveActivity();
+    //void testRemoveActivity();
 
 };
 
@@ -33,21 +34,30 @@ void TestActivityList::testAddGetActivity() {
     ActivityList actList;
     Activity a1;
     Activity a2;
+    Category c;
+    std::list<Category> list;
+    std::list<Activity> activitylist;
 
     a1.setTask("Attività 1");
     a2.setTask("Attività 2");
 
-    actList.addActivity(a1);
-    actList.addActivity(a2);
+    actList.addCategory(c);
 
-    QVERIFY (actList.getActivity().size() == 2);
-    QVERIFY ((*actList.getActivity().begin())->getTask() == "Attività 1");
+    actList.getCategory(list);
 
-    auto i = std::next(actList.getActivity().begin(), 1);
-    QVERIFY ((*i)->getTask() == "Attività 2");
+    actList.addActivity(c,a1);
+    actList.addActivity(c,a2);
+
+    c.getActivity(activitylist);
+
+    QVERIFY (activitylist.size()==2);
+    QVERIFY ((activitylist.begin())->getTask() == "Attività 1");
+
+    auto i = std::next(activitylist.begin(), 1);
+    QVERIFY ((*i).getTask() == "Attività 2");
 }
 
-void TestActivityList::testRemoveActivity() {
+/*void TestActivityList::testRemoveActivity() {
     ActivityList actList;
     auto a1 = new Activity();
     auto a2 = new Activity();
@@ -65,7 +75,7 @@ void TestActivityList::testRemoveActivity() {
     QVERIFY(actList.getActivity().empty());
 
 }
-
+*/
 QTEST_APPLESS_MAIN(TestActivityList)
 
 #include "tst_testactivitylist.moc"
