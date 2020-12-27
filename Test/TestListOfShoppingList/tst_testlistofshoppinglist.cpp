e35e4424a2ade45ea9fd4cd075fa45a7ab301f82
marco <marco.deluca4@stud.unifi.it>
2020-12-27 17:04:02 +0100

    Aggiornato e completato test list of shoppinglist

--- Test/TestListOfShoppingList/tst_testlistofshoppinglist.cpp
+++ Test/TestListOfShoppingList/tst_testlistofshoppinglist.cpp
@@ -33,38 +33,45 @@ void TestListOfShoppingList::testAddGetShoppingList()
     ListOfShoppingList listOfShoppingList;
     ShoppingList sh1;
     ShoppingList sh2;
+    std::list<ShoppingList> list;
 
     sh1.setNameList("Test1");
     sh2.setNameList("Test2");
 
-    listOfShoppingList.addShoppingList(&sh1);
-    listOfShoppingList.addShoppingList(&sh2);
+    listOfShoppingList.addShoppingList(sh1);
+    listOfShoppingList.addShoppingList(sh2);
 
-    QVERIFY (listOfShoppingList.getList().size() == 2);
-    QVERIFY ((*listOfShoppingList.getList().begin())->getNameList() == "Test1");
+    listOfShoppingList.getList(list);
 
-    auto i = std::next(listOfShoppingList.getList().begin(), 1);
-    QVERIFY ((*i)->getNameList() == "Test2");
+    QVERIFY (list.size() == 2);
+    QVERIFY (list.begin()->getNameList() == "Test1");
+
+    auto i = std::next(list.begin(), 1);
+    QVERIFY ((i)->getNameList() == "Test2");
 
 }
 
 void TestListOfShoppingList::testRemoveShoppingList()
 {
     ListOfShoppingList listOfShoppingList;
-    auto sh1 = new ShoppingList;
-    auto sh2 = new ShoppingList;
+    ShoppingList sh1;
+    ShoppingList sh2;
+    std::list<ShoppingList> list;
 
-    sh1->setNameList("Test1");
-    sh2->setNameList("Test2");
+    sh1.setNameList("Test1");
+    sh2.setNameList("Test2");
 
     listOfShoppingList.addShoppingList(sh1);
     listOfShoppingList.addShoppingList(sh2);
 
     listOfShoppingList.removeShoppingList(sh2);
-    QVERIFY(listOfShoppingList.getList().size() == 1);
+    listOfShoppingList.getList(list);
+    QVERIFY(list.size() == 1);
 
     listOfShoppingList.removeShoppingList(sh1);
-    QVERIFY(listOfShoppingList.getList().empty());
+    list.clear();
+    listOfShoppingList.getList(list);
+    QVERIFY(list.empty());
 
 }
 
